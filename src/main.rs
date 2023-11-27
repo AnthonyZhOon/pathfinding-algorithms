947e7a097a54bef644a57ae46fcbfff1169bf2bd
Anthony Oon <anthonyoon@192-168-1-107.tpgi.com.au>
2023-11-27 20:50:07 +1100

    Working on partial observable probabilistic algorithm

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-
 use pathfinding::prelude::astar;
 use pathfinding::prelude::Grid;
 use plotters::coord::Shift;
@@ -13,12 +12,13 @@ fn plot_grid(
 )-> Result<(), Box<dyn std::error::Error>>{
     let sub_areas = drawing_area.split_evenly((grid.width, grid.height));
     let red = 150;
+    const BLUE: plotters::style::RGBColor = RGBColor(59, 132, 227);
     for (idx, sub_area) in (0..).zip(sub_areas.iter()) {
         if grid.has_vertex((idx % grid.width, idx / grid.width)) {
-            sub_area.fill(&RGBColor(red, 100, 100))?;
+            sub_area.fill(&WHITE)?;
             // sub_area.titled(&idx.to_string(), ("sans-serif", 30))?;
         } else {
-            sub_area.fill(&BLACK)?;
+            sub_area.fill(&GREY_A200)?;
         }
     }
     if let Some(path) = path {
@@ -51,43 +51,84 @@ fn random_grid(width: usize, height: usize, seed: u64) -> Grid {
 }
 
 pub mod search_algorithms;
+use plotters::style::full_palette::GREY_A200;
 use search_algorithms::bfs::bfs;
 use search_algorithms::dfs::dfs;
+use ndarray;
+
+fn detect(ground: Grid, mut perceived: Grid, (x, y): (usize, usize), kernel: ndarray::Array2<bool>) {
+    // The Kernel should be centred on the location
+    // x - nrows//2 + i
+    // y - ncols//2 + j
+    for i in 0..kernel.nrows() {
+        for j in 0..kernel.ncols() {
+            if kernel[[i, j]] {
+                let row = x - kernel.nrows()/2 + i;
+                let col = y - kernel.ncols()/2 + j;
+                if row in  {
+                    match ground.has_vertex((row, col)) {
+                        true => ,
+                        false =>,
+                    }}
+            }
+        }
+    }
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     use rand::Rng;
-    let grid = random_grid(1000, 1000, 131);
+    let grid = random_grid(100, 100, 131);
     let mut rng = rand::thread_rng();
     let goal = grid.iter().nth(rng.gen_range(0..grid.vertices_len())).unwrap();
     let start = grid.iter().nth(rng.gen_range(0..grid.vertices_len())).unwrap();
-    let successors_weighted = |p: &(usize, usize)| grid.neighbours(*p).iter().map(|&p| (p, 20*p.0)).collect::<Vec<_>>();
+    let successors_weighted = |p: &(usize, usize)| grid.neighbours(*p).iter().map(|&p| (p, 1)).collect::<Vec<_>>();
     let mut successors = |p: &(usize, usize)| grid.neighbours(*p);
     let mut success = |p: &(usize, usize)| *p == goal;
-
+    let mut heuristic = |p| grid.distance(*p, goal);
     let result_astar:Option<(Vec<(usize, usize)>, usize)>  = astar(
                         &start,
                          successors_weighted,
-                         |p| grid.distance(*p, goal),
+                         heuristic,
                           |p| *p == goal
                         );
     match result_astar {
-        Some((path, _cost)) => save_result(Some(path), grid.clone(), start, goal, "astar.png")?,
-        None => save_result(None, grid.clone(), start, goal, "astar.png")?
+        Some((path, _cost)) => save_result(Some(path), grid.clone(), start, goal, "astar.png", "Ground truth")?,
+        None => save_result(None, grid.clone(), start, goal, "astar.png", "Ground Truth (Impossible)")?
     }
-                
-    let result_bfs: Option<Vec<(usize, usize)>> = bfs(start, &mut successors, &mut success);
-    // let result_dfs = dfs(start, &mut successors, &mut success);
 
-    match result_bfs {
-        Some(path) => save_result(Some(path), grid.clone(), start, goal, "bfs.png")?,
-        None => save_result(None, grid.clone(), start, goal, "bfs.png")?
+    // Take the ground truth and run a convolution with noise to create a probability interpretation of the grid
+
+    // Use the probability map to instantiate world-hypotheses and plan on them
+
+    // Step through the proposed solutions, voting on the next move and updating agent's map of the world?
+    // Replan either every step or when encountering newly perceived terrain/lack of
+    // 
+    // Run the simulation
+    let mut current = start.clone();
+    let guess = random_grid(100, 100, 2);
+    let mut steps: usize = 0;
+    while !success(&current) {
+        let guess_astar = astar(&start, successors_weighted, heuristic, |p| *p == goal);
+
+        match guess_astar {
+            Some((path, _cost)) => {save_result(Some(path), guess.clone(), start, goal, "astar.png", &format!("Guessed path {steps}"))?;
+                                                                current = path.first().unwrap().clone()},
+            None => {save_result(None, guess.clone(), start, goal, "astar.png", &format!("Guessed path {steps} (impossible)"))?; break},
+        }
+        // Take 1 step and update position and visible graph
+        
+        steps += 1;
     }
-    // match result_dfs {
-    //     Some(path) => save_result(Some(path), grid.clone(), start, goal, "dfs.png")?,
-    //     None => save_result(None, grid.clone(), start, goal, "dfs.png")?
-    // }
+
     
+                
+    // let result_bfs: Option<Vec<(usize, usize)>> = bfs(start, &mut successors, &mut success);
+
+    // match result_bfs {
+    //     Some(path) => save_result(Some(path), grid.clone(), start, goal, "bfs.png")?,
+    //     None => save_result(None, grid.clone(), start, goal, "bfs.png")?
+    // }
 
     Ok(())
 }
@@ -97,6 +138,7 @@ fn save_result(
     grid: Grid, start: (usize, usize),
     goal: (usize, usize),
     file_name: &str,
+    title: &str,
 ) -> Result<(), Box<dyn std::error::Error>> {
     // let _ = plot_path(result.unwrap().0);
     const OUT_FILE_DIR: &str = "plotters-doc-data/";
@@ -104,7 +146,7 @@ fn save_result(
     let root = BitMapBackend::new(&out_file_name, (1920, 1080)).into_drawing_area();
     root.fill(&RGBColor(100,100,100))?;
     let root = root
-    .titled("Path Finding Grid", ("sans-serif", 60))?
+    .titled(title, ("sans-serif", 60))?
     .shrink(((1920 - 1080) / 2, 0), (1000, 1000));
     match result {
         Some(path) => plot_grid(&grid, &root, &start, &goal, Some(&path))?,
@@ -114,6 +156,33 @@ fn save_result(
     root.present().expect("Unable to write result to file, please make sure 'plotters-doc-data' dir exists under current dir");
     println!("Result has been saved to {}", out_file_name);
     Ok(())
+}
+
+mod safe_interval_astar {
+    use pathfinding::prelude::Grid;
+    use super::random_grid;
+    use std::collections::HashMap;
+    type Pos = (usize, usize);
+    // Generate a grid, apply the dynamic obstacles to create safe intervals
+    // Run djikstra_all for a heuristic
+    // Create the neighbours function
+    struct PosWithIntervals {
+        position: Pos,
+        safe_intervals: Vec<(usize, usize)>
+    }
+
+    struct NodeWithInterval(Pos, usize);
+
+    fn prepare_grid() {
+        let grid: Grid = random_grid(300, 300, 10);
+        let mut interval_grid: HashMap<Pos, PosWithIntervals> = HashMap::new();
+        for vertex in grid.iter() {
+            interval_grid.insert(vertex, PosWithIntervals { position: vertex, safe_intervals: vec![(0, 99)] });
+        }
+    }
+
+    
+
 }
 
 mod safe_interval_path_planning {
